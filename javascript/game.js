646b7c566a7aa3a0029bf147f76bbb645c5679ea
Chaucee <hi@chaucee.com>
2017-12-12 17:16:38 -0700

    working app

--- javascript/game.js
+++ javascript/game.js
@@ -1,39 +1,62 @@
-var topics = ["dog", "cat", "parrot", "giraffe", "cheetah", "goldfish", "bird", "kangaroo", "owl", "dolphin", "killer whale"]
+// Create Topics Variable
+var topics = ["koala", "giraffe", "tasmanian devil", "cheetag", "kangaroo", "wombat", "elephant", "platypus"]
 
-function displayAnimal() {
-    var animal = $(this).attr('.image-display');
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=JLVs3oGYrrFoQ1FRuOKC5Pl31trTkWEr&limit=15";
+// Display Buttons
+function renderButtons() {
+    $(".animal-buttons").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>");
+        a.attr("data-animal", topics[i]);
+        a.addClass("button");
+        a.text(topics[i]);
+        $(".animal-buttons").append(a);
+    }
+}
+
+renderButtons();
+
+// Function to display memes when button is clicked
+$(document).on('click', '.button', function() {
+
+    var animal = $(this).attr('data-animal');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=JLVs3oGYrrFoQ1FRuOKC5Pl31trTkWEr&limit=15";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).done(function(response) {
-        var animalDisplayDiv = $('.image-display').append(displayAnimal);
 
+        var results = response.data;
 
-    })
-}
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $(".image-display");
+            var rating = results[i].rating;
+            var paragraph = $('<p>').text('Rating: ' + rating);
 
-// Display Animal Memes
-function renderButtons() {
-    $(".animal-buttons").empty();
-    for (var i = 0; i < animal.length; i++) {
-        var a = $("<button>");
-        a.text(animal[i]);
-        $(".animal-buttons").append(a);
-        
-    }
-}
+            var animalImage = $('<img>');
+            animalImage.attr('src', results[i].images.fixed_height_still.url);
+            animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+            animalImage.attr("data-animate", results[i].images.fixed_height.url);
+            animalImage.attr("data-state", "still");
+            animalImage.addClass("img-responsive cartoonGif");
+            gifDiv.prepend(paragraph);
+            gifDiv.prepend(animalImage);
 
+            $('.image-display').prepend(gifDiv);
+
+        }
+
+    });
 
 // Animal Search and Submit
-$("#add-animal").on("click", function(event) {
+$('#add-animal').on("click", function(event) {
 event.preventDefault();
-var animal = $("#animal-input").val().trim();
-animal.push(animal);
+var userInput = $("#animal-input").val().trim();
+
+topics.push(userInput);
+
 renderButtons();
 });
 
-$(document).on("click", ".movie", displayAnimal);
 
-renderButtons();
+});
